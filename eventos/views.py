1b06f277ccbb3cc4452ebb9a2fc782a3c697320f
Dumbo4nonimo <144817276+Dumbo4nonimo@users.noreply.github.com>
2026-09-18 17:39:52 -0500

    Agregar mensaje de bienvenida en ruta raiz

--- eventos/views.py
+++ eventos/views.py
@@ -7,3 +7,10 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def health(request):
     return Response({"status": "ok", "message": "El servidor esta funcionando correctamente"})
+
+@api_view(['GET'])
+def home(request):
+    return Response({
+        "mensaje": "API del Organizador de Eventos Independientes",
+        "endpoints_disponibles - ir a": ["https://mini-proyecto-integrador1-back.onrender.com/api/health/"]
+    })
